38e410c8b00e64750936f210e6cd9ca3fa4e5277
Vasuaggarwal0509 <127047852+Vasuaggarwal0509@users.noreply.github.com>
2023-09-01 23:48:31 +0530

    word_guess_game.py
    no further

---
+++ word_guess_game.py
@@ -0,0 +1,68 @@
+import random
+import time
+
+
+# convert list into string
+def convert(space_list):
+    new=''
+    for x in space_list:
+        new+=x
+    return new
+def find_index(letter,word):
+
+    place=word.find(letter)
+    return place
+
+print('Welcome to Hangman game by Vasu Ji and Nariyal Annaa\n')
+time.sleep(1)
+name = str(input('Enter your name: '))
+time.sleep(0.5)
+print(f'Ram Ram ji  {name}! ji  Best of Luck! ji\n')
+print('The game is about to start!\n')
+time.sleep(3)
+print('Let\'s play Hangman!')
+
+
+
+
+words=['kids','rythm','poem','system','washing','computer','nails','box','go','image','hairs','ears','mouse','chairs']
+
+# word=random.choice(words)
+word=random.choice(words)
+display="_"*len(word)
+
+print(f'This is Hangman Word:  '+display+'enter your guess:')
+
+space_list=list(display)
+
+life=5
+while life!=0:
+    print('enter your letter')
+    letter=str(input())
+    if letter in word:
+        
+        place_2=find_index(letter,word)
+        space_list[place_2]=letter
+        time.sleep(1)
+        print(f"Aiyoo! Sahi jawab ji {convert(space_list)} Guess karo ji")
+
+        
+    else:
+        life-=1
+
+        print("AAiyoo! Ramaa ....\n")
+
+
+        time.sleep(1)
+        print('galat guess h ji\n')
+        time.sleep(1)
+        print(f'abb apno pass bachi h {life} life\n\n')
+        print(f'abhi ji apnoo pass ye {convert(space_list)} h  '+'ab guess karoo ji')
+
+    if '_'not in space_list:
+        time.sleep(4)
+        print('you win    \n\n'+'7 crore ......jeet gaye ji \n\nAb toh party Mangta h .. ,Annaa..')
+        exit()
+
+if '_'in space_list and life==0:
+    print(f'you loose'+'The word was {word}')
